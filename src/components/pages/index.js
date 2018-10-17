e76907e0bcba36d80dfbf40e504a112b52db9cad
SAsachov <sergii.asachov@nuance.com>
2018-10-17 18:01:10 +0300

    index.js

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,5 +1,5 @@
-import Cart from "./Cart";
-import Catalog from "./Catalog";
-import Order from "./Order";
+import Cart from "./Cart/Cart";
+import Catalog from "./Catalog/Catalog";
+import Order from "./Order/Order";
 
 export { Cart, Catalog, Order };
