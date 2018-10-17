7969e6b462888ee42044745656ab3c4e4a041433
SAsachov <sergii.asachov@nuance.com>
2018-10-17 17:56:14 +0300

    add pages

---
+++ src/components/controls/index.js
@@ -0,0 +1,4 @@
+import Button from "./Button/Button";
+import OkButton from "./Button/OkButton";
+
+export { Button, OkButton };
---
+++ src/components/pages/index.js
@@ -0,0 +1,5 @@
+import Cart from "./Cart";
+import Catalog from "./Catalog";
+import Order from "./Order";
+
+export { Cart, Catalog, Order };
