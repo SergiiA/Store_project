6b1803980d457360f8e9856056bbf7d6d2750198
SAsachov <sergii.asachov@nuance.com>
2018-10-19 18:09:17 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import store from "store/store";
+import { Provider } from "react-redux";
 import DefaultLayout from "./components/layouts/DefaultLayout";
+
 import Catalog from "./components/pages/Catalog/Catalog";
+import Order from "./components/pages/Order/Order";
+import Cart from "./components/pages/Cart/Cart";
+
 import Button from "./components/controls/Button/Button";
-import OkButton from "./components/controls/Button/OkButton";
 
 class App extends Component {
   render() {
@@ -17,7 +23,17 @@ class App extends Component {
           </DefaultLayout>
           <div>
             <Button />
-            <OkButton />
+          </div>
+          <div className="Catalog" style={{ display: "inline" }}>
+            <Provider store={store}>
+              <BrowserRouter>
+                <Switch>
+                  <Route path="/" exact component={Catalog} />
+                  <Route path="/order" component={Order} />
+                  <Route path="/cart" component={Cart} />
+                </Switch>
+              </BrowserRouter>
+            </Provider>
           </div>
         </header>
       </div>
