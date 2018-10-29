c671b8b16689a7f019c87f0436fd0e7fb5ade1b6
SAsachov <sergii.asachov@nuance.com>
2018-10-29 15:58:09 +0200

    config

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,33 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ManifestPlugin = require('webpack-manifest-plugin');
-const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ManifestPlugin = require("webpack-manifest-plugin");
+const DirectoryNamedWebpackPlugin = require("directory-named-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
-const mode = (process || process.env.npm_lifecycle_script.match(/(?<=--mode\s+).+/) || ['production'])[0];
+const mode = (process ||
+  process.env.npm_lifecycle_script.match(/(?<=--mode\s+).+/) || [
+    "production"
+  ])[0];
 
 module.exports = {
   mode,
-  entry: './src/index.jsx',
+  entry: "./src/index.jsx",
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
-    publicPath: '/',
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+    publicPath: "/"
   },
   resolve: {
-    modules: ['node_modules', './src'],
-    extensions: ['.js', '.jsx', '.json'],
-    plugins: [new DirectoryNamedWebpackPlugin(true)],
+    modules: ["node_modules", "./src"],
+    extensions: [".js", ".jsx", ".json"],
+    plugins: [new DirectoryNamedWebpackPlugin(true)]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Sports Store',
+      title: "Sports Store",
       inject: true,
-      favicon: './src/favicon.png',
-      template: './src/index.ejs',
+      favicon: "./src/favicon.png",
+      template: "./src/index.ejs",
       minify: {
         removeComments: true,
         collapseWhitespace: true,
@@ -35,42 +38,42 @@ module.exports = {
         keepClosingSlash: true,
         minifyJS: true,
         minifyCSS: true,
-        minifyURLs: true,
-      },
+        minifyURLs: true
+      }
     }),
     new ManifestPlugin({
-      fileName: 'asset-manifest.json',
+      fileName: "asset-manifest.json"
     }),
-    new CleanWebpackPlugin(['build']),
+    new CleanWebpackPlugin(["build"])
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: ['babel-loader'],
-        exclude: /node_modules/,
+        use: ["babel-loader"],
+        exclude: /node_modules/
       },
       {
         test: /\.(eot|otf|svg|ttf|woff|png)$/,
         use: {
-          loader: 'file-loader',
-          options: { name: 'assets/[name][hash].[ext]' },
-        },
-      },
-    ],
+          loader: "file-loader",
+          options: { name: "assets/[name][hash].[ext]" }
+        }
+      }
+    ]
   },
-  devtool: mode === 'production' ? false : 'cheap-module-source-map',
+  devtool: mode === "production" ? false : "cheap-module-source-map",
   node: {
-    dgram: 'empty',
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-    child_process: 'empty',
+    dgram: "empty",
+    fs: "empty",
+    net: "empty",
+    tls: "empty",
+    child_process: "empty"
   },
   devServer: {
     contentBase: false,
     historyApiFallback: true,
-    host: '0.0.0.0',
-    port: 1000,
-  },
+    host: "0.0.0.0",
+    port: 3000
+  }
 };
